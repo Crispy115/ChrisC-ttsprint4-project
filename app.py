0140b11847ea9256d39eaab53f4c8706816d1f0c
Crispy115 <167049155+Crispy115@users.noreply.github.com>
2024-04-18 22:49:27 -0400

    commit

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
-st.header('hello world')
+
+st.header('Market of used cars.Original data')
+st.write ('Filter the data below to see the ads by manufacturer')
+
+
+df = pd.read_csv('vehicles_us.csv')
+df = df.drop(df.columns[0], axis=1)
+
+
+manufacturer = df['manufacturer_name'].unique() 
+
+select_manufacturer = st.selectbox('Select a Manufacturer', manufacturer)
+
+df_filtered = df[ df.manufacturer_name == selected_manufacturer ]
+
+df_filtered
